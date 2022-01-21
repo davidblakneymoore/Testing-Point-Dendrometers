33bc0d7ba2c247c972d2b1f16c8a14cf5a8bf5ba
David Moore <46967676+davidblakneymoore@users.noreply.github.com>
2022-01-21 13:18:02 -0500

    Add files via upload

---
+++ Testing the Point Dendrometers.R
@@ -0,0 +1,36 @@
+
+# Testing the Point Dendrometers
+
+# David Moore
+# January 21st, 2022
+
+
+# Explanation
+
+# This R script should be used in conjunction with the 'Testing the Point
+# Dendrometers' CRBasic script.
+
+# It's also intended to be used with the plexiglass device for keeping all the
+# point dendrometer plungers depressed the same amount and for holding all the
+# point dendrometer plungers at a constant length.
+
+# You may need to expand the 'Plots' part of the RStudio window for the figure.
+
+
+# Constants
+
+Working_Directory <- "C:\\Campbellsci\\LoggerNet"
+
+
+# The Program
+
+setwd(Working_Directory)
+Point_Dendrometer_Data <- read.table("CR1000_4_Point_Dendrometers.dat", skip = 1, header = T, sep = ",")
+Point_Dendrometer_Data <- Point_Dendrometer_Data[-c(1:2), ]
+Point_Dendrometer_Data$TIMESTAMP <- as.POSIXct(Point_Dendrometer_Data$TIMESTAMP, format = "%Y-%m-%d %H:%M:%S")
+par(mar = c(10, 4, 4, 2), xpd = T)
+matplot(Point_Dendrometer_Data$TIMESTAMP, Point_Dendrometer_Data[, grep("Point_Dendrometer_Reading", colnames(Point_Dendrometer_Data))], xaxt = 'n', yaxt = 'n', main = "Testing the Point Dendrometers", pch = 19, col = rainbow(length(colnames(Point_Dendrometer_Data[grep("Point_Dendrometer_Reading", colnames(Point_Dendrometer_Data))]))), ylab = "Sensor Output (mV)", xlab = "Time")
+Axis_Ranges <- par('usr')
+axis.POSIXct(1, pretty(Point_Dendrometer_Data$TIMESTAMP)[pretty(Point_Dendrometer_Data$TIMESTAMP) > Axis_Ranges[1] & pretty(Point_Dendrometer_Data$TIMESTAMP) < Axis_Ranges[2]], format = "%b. %d, %H:%M")
+axis(2, pretty(as.matrix(Point_Dendrometer_Data[, grep("Point_Dendrometer_Reading", colnames(Point_Dendrometer_Data))]))[pretty(as.matrix(Point_Dendrometer_Data[, grep("Point_Dendrometer_Reading", colnames(Point_Dendrometer_Data))])) > Axis_Ranges[3] & pretty(as.matrix(Point_Dendrometer_Data[, grep("Point_Dendrometer_Reading", colnames(Point_Dendrometer_Data))])) < Axis_Ranges[4]])
+legend("bottom", legend = gsub("[.]", "", gsub("Point_Dendrometer_Reading.", "", colnames(Point_Dendrometer_Data[grep("Point_Dendrometer_Reading", colnames(Point_Dendrometer_Data))]))), title = "Point Dendrometer Number", pch = 19, col = rainbow(length(colnames(Point_Dendrometer_Data[grep("Point_Dendrometer_Reading", colnames(Point_Dendrometer_Data))]))), horiz = T, inset = c(0, -0.25))
